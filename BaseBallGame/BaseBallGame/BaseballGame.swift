bcead5b5478da1d456c52e852400d6e123e4648c
sy0201 <noey0201@gmail.com>
2024-11-05 17:51:34 +0900

    [FEAT] : Lv2 3자리수 입력후 스트라이크,볼 처리 구현

--- BaseBallGame/BaseBallGame/BaseballGame.swift
+++ BaseBallGame/BaseBallGame/BaseballGame.swift
@@ -7,21 +7,31 @@
 
 import Foundation
 
-final class BaseballGame {
-    func startGame() {
-        let answer = makeAnswer()
-        print("answer : \(answer)")
-    }
-    
-    func makeAnswer() -> [Int] {
-        // Set을 사용하여 3개의 숫자 중 중복값 자동으로 걸러내기
-        var numbers: Set<Int> = []
-        
-        while numbers.count < 3 {
-            let randomInt = Int.random(in: 1...9)
-            numbers.insert(randomInt)
-        }
-        
-        return Array(numbers)
-    }
-}
+//let game = BaseballGame()
+//game.startGame()
+//
+//let input = readLine()
+//
+//final class BaseballGame {
+//    func startGame() {
+//        let answer = makeAnswer()
+//        print("< 게임을 시작합니다 >")
+//        print("숫자를 입력하세요")
+//        print("answer : \(answer)")
+//    }
+//    
+//    func makeAnswer() -> String {
+//        // Set을 사용하여 3개의 숫자 중 중복값 자동으로 걸러내기
+//        var numbers: Set<Int> = []
+//        
+//        while numbers.count < 3 {
+//            let randomInt = Int.random(in: 1...9)
+//            numbers.insert(randomInt)
+//        }
+//        
+//        // randomInt의 3개 수를 map함수로 Int를 String으로 변환
+//        let result = numbers.map { String($0) }.joined()
+//        
+//        return result
+//    }
+//}
