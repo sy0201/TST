590db4f25db8fb414320a4262831f54ace8311fb
sy0201 <noey0201@gmail.com>
2024-11-13 21:26:02 +0900

    [INIT] : 프로젝트 초기화

---
+++ TS_AppCalculator/TS_AppCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TS_AppCalculator
+//
+//  Created by siyeon park on 11/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
