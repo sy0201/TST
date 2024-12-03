86556388e355fb42643fffc34f4b3a54b5613fc9
sy0201 <noey0201@gmail.com>
2024-12-03 14:16:03 +0900

    [INIT] : 프로젝트 초기화

---
+++ TS_Weather/TS_Weather/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  TS_Weather
+//
+//  Created by siyeon park on 12/3/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
