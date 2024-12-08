6970e38cb1eec8a60bd7e0f4ad9c5306a14dfcf2
sy0201 <noey0201@gmail.com>
2024-12-08 13:07:28 +0900

    [FEAT] : 친구 목록 UI 구현

--- TS_PoketmonContact/TS_PoketmonContact/Support/SceneDelegate.swift
+++ TS_PoketmonContact/TS_PoketmonContact/Support/SceneDelegate.swift
@@ -14,9 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ContactListViewController()
+        let viewController = ContactListViewController()
+        
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
