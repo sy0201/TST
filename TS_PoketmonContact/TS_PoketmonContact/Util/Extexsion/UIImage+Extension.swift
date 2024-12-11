957aa5b61c8357782cb549d89945aca1cc29a548
sy0201 <noey0201@gmail.com>
2024-12-11 15:12:46 +0900

    [FIX] : 이미지 로드시 백그라운드에서 호출되지 않게 코드 수정

--- TS_PoketmonContact/TS_PoketmonContact/Util/Extexsion/UIImage+Extension.swift
+++ TS_PoketmonContact/TS_PoketmonContact/Util/Extexsion/UIImage+Extension.swift
@@ -5,18 +5,22 @@
 //  Created by siyeon park on 12/10/24.
 //
 
-import Alamofire
+import Kingfisher
 import UIKit
 
 extension UIImageView {
     func loadImage(from urlString: String) {
         guard let url = URL(string: urlString) else { return }
         
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self?.image = image
-                }
+        // Kingfisher로 이미지 비동기 로드
+        self.kf.setImage(with: url, options: [
+            .cacheOriginalImage  // 원본 이미지를 캐시하도록 설정
+        ]) { result in
+            switch result {
+            case .success(let value):
+                print("이미지 로딩 성공: \(value.image)")
+            case .failure(let error):
+                print("이미지 로딩 실패: \(error.localizedDescription)")
             }
         }
     }
