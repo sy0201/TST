55493721eae48a4c8c132a95db85de528e800045
sy0201 <noey0201@gmail.com>
2024-12-12 19:06:46 +0900

    [REFACTOR] : 네트워크 연결체크 중복코드 Extension으로 리팩토링

--- TS_PoketmonContact/TS_PoketmonContact/Screen/ContactList/ContactListViewController.swift
+++ TS_PoketmonContact/TS_PoketmonContact/Screen/ContactList/ContactListViewController.swift
@@ -35,43 +35,6 @@ final class ContactListViewController: UIViewController {
         contactViewModel.loadContacts()
         contactListView.tableView.reloadData()
     }
-    
-    func checkNetworkStatus() {
-        if NetworkMonitor.shared.isConnected {
-            print("인터넷 연결됨...")
-        } else {
-            print("인터넷 연결안됨...")
-            checkNetworkAlert()
-        }
-    }
-
-    func checkNetworkAlert() {
-        let alertController = UIAlertController(
-            title: "네트워크에 접속할 수 없습니다.",
-            message: "네트워크 연결 상태를 확인해주세요.",
-            preferredStyle: .alert
-        )
-        
-        let endAction = UIAlertAction(title: "종료", style: .destructive) { _ in
-            // 앱 종료
-            UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                exit(0)
-            }
-        }
-        
-        let confirmAction = UIAlertAction(title: "확인", style: .default) { _ in
-            // 설정앱 켜주기
-            guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
-            if UIApplication.shared.canOpenURL(url) {
-                UIApplication.shared.open(url)
-            }
-        }
-        
-        alertController.addAction(endAction)
-        alertController.addAction(confirmAction)
-        self.present(alertController, animated: true, completion: nil)
-    }
 
     func setupNavigationBar() {
         // UINavigationBarAppearance 설정
--- TS_PoketmonContact/TS_PoketmonContact/Screen/PhoneBook/PhoneBookViewController.swift
+++ TS_PoketmonContact/TS_PoketmonContact/Screen/PhoneBook/PhoneBookViewController.swift
@@ -97,44 +97,7 @@ final class PhoneBookViewController: UIViewController {
         
         navigationController?.popViewController(animated: true)
     }
-    
-    func checkNetworkStatus() {
-        if NetworkMonitor.shared.isConnected {
-            print("인터넷 연결완료")
-        } else {
-            print("인터넷 연결실패")
-            checkNetworkAlert()
-        }
-    }
-    
-    func checkNetworkAlert() {
-        let alertController = UIAlertController(
-            title: "네트워크에 접속할 수 없습니다.",
-            message: "네트워크 연결 상태를 확인해주세요.",
-            preferredStyle: .alert
-        )
-        
-        let endAction = UIAlertAction(title: "종료", style: .destructive) { _ in
-            // 앱 종료
-            UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                exit(0)
-            }
-        }
-        
-        let confirmAction = UIAlertAction(title: "확인", style: .default) { _ in
-            // 설정앱 켜주기
-            guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
-            if UIApplication.shared.canOpenURL(url) {
-                UIApplication.shared.open(url)
-            }
-        }
-        
-        alertController.addAction(endAction)
-        alertController.addAction(confirmAction)
-        self.present(alertController, animated: true, completion: nil)
-    }
-    
+
     func validInputAlert(title: String, message: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let confirmAction = UIAlertAction(title: "확인", style: .default)
