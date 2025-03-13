5ee2b0b67e7d57ae683244e7c20d8408cd724dd2
sy0201 <noey0201@gmail.com>
2025-03-13 17:18:31 +0900

    [Feat] 프로젝트 세팅

---
+++ LoginSystem_ParkSiYeon/LoginSystem_ParkSiYeon/Screen/Start/StartViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StartViewController.swift
+//  LoginSystem_ParkSiYeon
+//
+//  Created by siyeon park on 3/13/25.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
