c87197e112d7c53c1da78e7cdb2e0ef8088b5345
sy0201 <noey0201@gmail.com>
2025-03-15 22:39:14 +0900

    [Feat] LoginViewControllerUI 구현

---
+++ LoginSystem_ParkSiYeon/LoginSystem_ParkSiYeon/Screen/Login/LoginViewModel/LoginViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewModel.swift
+//  LoginSystem_ParkSiYeon
+//
+//  Created by siyeon park on 3/15/25.
+//
+
+import Foundation
+import RxSwift
+
+final class LoginViewModel {
+    // 상태 전달을 위한 Observable
+    let logoutSuccess = PublishSubject<Void>()
+    let withdrawSuccess = PublishSubject<Void>()
+    let nickname = BehaviorSubject<String>(value: "")
+    
+    private let userDefaultsManager = UserDefaultsManager.shared
+    private let coreDataManager = UserInfoCoreDataManager.shared
+    
+    init() {
+        if let savedNickname = userDefaultsManager.getNickname() {
+            nickname.onNext(savedNickname) // 사용자 이름을 BehaviorSubject에 전달
+        }
+    }
+    func logout() {
+        userDefaultsManager.clearUserLoggedIn()
+        userDefaultsManager.clearNickname()
+        userDefaultsManager.clearUserEmail()
+        
+        logoutSuccess.onNext(())
+    }
+    
+    func withdraw() {
+        guard let email = userDefaultsManager.getUserEmail() else {
+            return
+        }
+        
+        let coreDataManager = UserInfoCoreDataManager.shared
+        if let user = coreDataManager.fetchUserByEmail(email: email) {
+            coreDataManager.deleteUser(user: user)
+            
+            userDefaultsManager.clearUserLoggedIn()
+            userDefaultsManager.clearNickname()
+            userDefaultsManager.clearUserEmail()
+            
+            withdrawSuccess.onNext(())
+        }
+    }
+}
